b8ee009b1328e4f8ce3caa61235c7506cd025713
Bruno Cabrita <bruno.cabrita@metacriacoes.com>
2014-05-22 17:03:59 +0100

    vibrate plugin added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 /* Javascript */
+(function(window, undefined){
+	"use strict";
+	var document = window.document;
+
+	function vibrate(num){
+		navigator.notification.vibrate(num);
+	}
+
+	function init(){
+
+	}
+
+	window.app = {
+		vibrate : vibrate
+	};
+	window.onload = init;
+})(window);
