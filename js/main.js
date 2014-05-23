817b3463ade54860b9d9ce69f6fcfe6288c5ea26
Bruno Cabrita <bruno.cabrita@metacriacoes.com>
2014-05-23 11:16:17 +0100

    qr-scanner added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 /* Javascript */
 (function(window, undefined){
     "use strict";
-    var document = window.document,
+    var document = window.document,,
+        debugElem,
         hasFlashlight = false;
 
     function vibrate(num){
@@ -11,11 +12,12 @@
     function toggleFlashlight(elem){
         try{
             if(hasFlashlight){
-                //elem.innerHTML = "Flashlight (On)";
+                elem.innerHTML = "Flashlight (On)";
                 window.plugins.flashlight.toggle();
+                debugLog(JSON.stringify(window.plugins.flashlight));
             }
         } catch(err) {
-            console.log('ERROR:',err);
+            debugLog('ERROR: '+err.message);
         }
     }
 
@@ -29,18 +31,38 @@
               }
             });
         } catch(err) {
-            console.log('ERROR:',err);
+            debugLog('ERROR: '+err.message);
         }
     }
 
+    function scanQrCode(){
+        cordova.plugins.barcodeScanner.scan(
+          function (result) {
+              alert("We got a barcode\n" +
+                    "Result: " + result.text + "\n" +
+                    "Format: " + result.format + "\n" +
+                    "Cancelled: " + result.cancelled);
+          }, 
+          function (error) {
+              alert("Scanning failed: " + error);
+          }
+       );
+    }
+
+    function debugLog(str){
+        debugElem.innerHTML += str.toString();
+    }
+
     function init(){
-        console.log('init...');
+        debugElem = document.getElementById('debug');
+        debugLog('init...');
         testFlashlight();
     }
 
     window.app = {
         vibrate : vibrate,
-        toggleFlashlight : toggleFlashlight
+        toggleFlashlight : toggleFlashlight,
+        scanQrCode : scanQrCode
     };
     window.onload = init;
 })(window);
